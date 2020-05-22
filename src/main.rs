3254cd0d9ee175423a9b476f55bd3bb24b48d502
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-05-23 08:32:16 +0900

    Remove unnecessary stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-//! An example of generating julia fractals.
 extern crate image;
-extern crate num_complex;
 
 fn rawboard(square_size_in_pixel: f32) -> image::RgbImage {
     /* Numbers based on physical measurements */
@@ -19,69 +17,77 @@ fn rawboard(square_size_in_pixel: f32) -> image::RgbImage {
     /* first draw the board */
     let mut imgbuf = image::ImageBuffer::from_pixel(imgx, imgy, tak1_color);
 
-    // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let pixel = imgbuf.get_pixel_mut(x, y);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        /* the size of each square is 1.0 */
+        /* the center of the image is the origin */
+        let cx = x as f32 / square_size_in_pixel - cwidth / 2.;
+        let cy = y as f32 / square_size_in_pixel - cheight / 2.;
 
-            /* the size of each square is 1.0 */
-            /* the center of the image is the origin */
-            let cx = x as f32 / square_size_in_pixel - cwidth / 2.;
-            let cy = y as f32 / square_size_in_pixel - cheight / 2.;
-
-            /* the board is already drawn */            
-            /* then draw the squares */
-            if -1.5 <= cx && cx <= 1.5 && -1.5 <= cy && cy <= 1.5 {
-                *pixel = tam2hue_color;
+        /* the board is already drawn */
+        /* then draw the squares */
+        if -1.5 <= cx && cx <= 1.5 && -1.5 <= cy && cy <= 1.5 {
+            *pixel = tam2hue_color;
+        }
+        if 1.5 <= cx.abs() && cx.abs() <= 2.5 && 1.5 <= cy.abs() && cy.abs() <= 2.5 {
+            *pixel = tam2hue_color;
+        }
+        if (-2.5 <= cx && cx <= 2.5 && -0.5 <= cy && cy <= 0.5)
+            || (-2.5 <= cy && cy <= 2.5 && -0.5 <= cx && cx <= 0.5)
+        {
+            *pixel = if -0.5 <= cx && cx <= 0.5 && -0.5 <= cy && cy <= 0.5 {
+                tam2zo1_color
+            } else {
+                tam2nua2_color
             }
-            if 1.5 <= cx.abs() && cx.abs() <= 2.5 && 1.5 <= cy.abs() && cy.abs() <= 2.5 {
-                *pixel = tam2hue_color;
-            }
-            if (-2.5 <= cx && cx <= 2.5 && -0.5 <= cy && cy <= 0.5)
-                || (-2.5 <= cy && cy <= 2.5 && -0.5 <= cx && cx <= 0.5)
-            {
-                *pixel = if -0.5 <= cx && cx <= 0.5 && -0.5 <= cy && cy <= 0.5 {
-                    tam2zo1_color
-                } else {
-                    tam2nua2_color
-                }
-            }
+        }
 
-            /* Now draw the lines */
+        /* Now draw the lines */
 
-            /* horizontal and vertical */
-            for loc in vec![-4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5] {
-                if (loc - cx).abs() <= line_width / 2.0 && cy.abs() <= 4.5 + line_width / 2.0 {
-                    *pixel = line_color;
-                }
-                if (loc - cy).abs() <= line_width / 2.0 && cx.abs() <= 4.5 + line_width / 2.0 {
-                    *pixel = line_color;
-                }
-            }
-
-            /* tam2nua2 corners */
-            if (2.5 - line_width * 2. - cx.abs()).abs() <= line_width / 2.0 && cy.abs() <= 0.5 - line_width * 1.5 && 0.25 <= cy.abs()  {
+        /* horizontal and vertical */
+        for loc in vec![-4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5] {
+            if (loc - cx).abs() <= line_width / 2.0 && cy.abs() <= 4.5 + line_width / 2.0 {
                 *pixel = line_color;
             }
-            if (2.5 - line_width * 2. - cy.abs()).abs() <= line_width / 2.0 && cx.abs() <= 0.5 - line_width * 1.5 && 0.25 <= cx.abs()  {
-                *pixel = line_color;
-            }
-            if (0.5 - line_width * 2. - cy.abs()).abs() <= line_width / 2.0 && cx.abs() <= 2.5 - line_width * 1.5 && 2.25 <= cx.abs() {
-                *pixel = line_color;
-            }
-            if (0.5 - line_width * 2. - cx.abs()).abs() <= line_width / 2.0 && cy.abs() <= 2.5 - line_width * 1.5 && 2.25 <= cy.abs() {
-                *pixel = line_color;
-            }
-
-            /* tam2hue diagonal */
-            if (cx + cy).abs() <= line_width * std::f32::consts::FRAC_1_SQRT_2 && cx.abs() <= 2.5 {
-                *pixel = line_color;
-            }
-            if (cx - cy).abs() <= line_width * std::f32::consts::FRAC_1_SQRT_2 && cx.abs() <= 2.5 {
+            if (loc - cy).abs() <= line_width / 2.0 && cx.abs() <= 4.5 + line_width / 2.0 {
                 *pixel = line_color;
             }
         }
+
+        /* tam2nua2 corners */
+        if (2.5 - line_width * 2. - cx.abs()).abs() <= line_width / 2.0
+            && cy.abs() <= 0.5 - line_width * 1.5
+            && 0.25 <= cy.abs()
+        {
+            *pixel = line_color;
+        }
+        if (2.5 - line_width * 2. - cy.abs()).abs() <= line_width / 2.0
+            && cx.abs() <= 0.5 - line_width * 1.5
+            && 0.25 <= cx.abs()
+        {
+            *pixel = line_color;
+        }
+        if (0.5 - line_width * 2. - cy.abs()).abs() <= line_width / 2.0
+            && cx.abs() <= 2.5 - line_width * 1.5
+            && 2.25 <= cx.abs()
+        {
+            *pixel = line_color;
+        }
+        if (0.5 - line_width * 2. - cx.abs()).abs() <= line_width / 2.0
+            && cy.abs() <= 2.5 - line_width * 1.5
+            && 2.25 <= cy.abs()
+        {
+            *pixel = line_color;
+        }
+
+        /* tam2hue diagonal */
+        if (cx + cy).abs() <= line_width * std::f32::consts::FRAC_1_SQRT_2 && cx.abs() <= 2.5 {
+            *pixel = line_color;
+        }
+        if (cx - cy).abs() <= line_width * std::f32::consts::FRAC_1_SQRT_2 && cx.abs() <= 2.5 {
+            *pixel = line_color;
+        }
     }
+
     return imgbuf;
 }
 
